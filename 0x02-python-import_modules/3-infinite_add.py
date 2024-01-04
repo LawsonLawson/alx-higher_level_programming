eba2eb4816853a9f44fcce1aab64248d47b3cbf2
Lawson Pascal Israel <isreallawson@outlook.com>
2024-01-04 20:53:10 +0000

    fixed code output fail

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    """ Print the addition of all arguments."""
     import sys
 
     outcome = 0
     for args in range(len(sys.argv) - 1):
-        total += int(sys.argv[args + 1])
+        outcome += int(sys.argv[args + 1])
     print("{}".format(outcome))
