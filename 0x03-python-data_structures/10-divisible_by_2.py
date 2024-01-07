e606720da2234a2d110dfab5692b5ad5c2c9306e
Lawson Pascal Israel <isreallawson@outlook.com>
2024-01-07 23:36:31 +0000

    a function that finds all even numbers in a list

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    """
+    Finds all the even numbers in a list
+    """
+    new_list = []
+    for number in range(len(my_list)):
+        if my_list[number] % 2 == 0:
+            new_list.append(True)
+        else:
+            new_list.append(False)
+        return new_list
