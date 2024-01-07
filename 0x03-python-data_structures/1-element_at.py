c842cdb6a99c22ee23251f310cdd714d9b986600
Lawson Pascal Israel <isreallawson@outlook.com>
2024-01-07 17:20:49 +0000

    a function that retrives an element from a list like in C

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def element_at(my_list, idx):
+    """Retrives an element from a list like in C"""
+    for elements in my_list:
+        if idx < 0:
+            return None
+        elif idx > len(my_list):
+            return None
+        else:
+            return my_list[idx]
