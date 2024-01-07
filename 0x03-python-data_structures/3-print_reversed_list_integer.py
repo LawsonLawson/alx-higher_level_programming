a7fb8a75c4dd250ef3beea4d2390822e01ae055d
Lawson Pascal Israel <isreallawson@outlook.com>
2024-01-07 18:16:21 +0000

    so I made a copy of the orignal list, reversed the copy and left the orignal list untouched

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
     """Prints all integers of a list in reverse order"""
-    my_list.reverse()
-    for integers in my_list:
-        print('{}'.format(integers))
+    my_list_copy = my_list.copy()
+    my_list_copy.reverse()
+    for integers in my_list_copy:
+        print('{:d}'.format(integers))
