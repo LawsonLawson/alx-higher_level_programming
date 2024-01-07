9c1fee23d5629081edb2fd56a7918450bf807625
Lawson Pascal Israel <isreallawson@outlook.com>
2024-01-07 17:03:44 +0000

    forgot to add the :d specifier

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -2,4 +2,4 @@
 def print_list_integer(my_list=[]):
     """Prints all integers of a list"""
     for integers in my_list:
-        print('{}'.format(integers))
+        print('{:d}'.format(integers))
