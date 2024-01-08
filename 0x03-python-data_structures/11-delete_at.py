49ec87173622e2df93a7c745dd6be37548d96de2
Lawson Pascal Israel <isreallawson@outlook.com>
2024-01-08 00:03:07 +0000

    added further checks for case handling

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -3,8 +3,13 @@ def delete_at(my_list=[], idx=0):
     """
     Deletes the item at a specified position in a list
     """
+    bounds = len(my_list)
     if my_list:
         if idx < 0:
+            return my_list
+        elif idx >= bounds:
+            return my_list
+        elif my_list is None:
             return my_list
         else:
             del my_list[idx]
