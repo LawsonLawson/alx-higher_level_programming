2580894a4c26903b9f9a1336b0ef6ace0d50067b
Lawson Pascal Israel <isreallawson@outlook.com>
2024-01-08 00:13:18 +0000

    fixed output code fail

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -4,11 +4,6 @@ def delete_at(my_list=[], idx=0):
     Deletes the item at a specified position in a list
     """
     bounds = len(my_list)
-    if my_list:
-        if idx < 0 and my_list is None:
-            return my_list
-        elif idx >= bounds:
-            return my_listt
-        else:
-            del my_list[idx]
-        return my_list
+    if my_list and 0 <= idx < bounds:
+        del my_list[idx]
+    return my_list
