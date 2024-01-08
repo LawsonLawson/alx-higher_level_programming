4d34069179f2fc42d53f4a57ad08bca481ab477c
Lawson Pascal Israel <isreallawson@outlook.com>
2024-01-08 13:43:44 +0000

    trying to fix the output code fail

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -3,7 +3,7 @@ def max_integer(my_list=[]):
     """
     Finds the biggest integer of a list
     """
-    if my_list == '' or my_list is None:
+    if my_list == '' or len(my_list) == 0:
         return None
     my_list.sort()
     return my_list[-1]
