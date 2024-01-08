c0426328ad4aeefe14c3c97270cd17d0e15f1fe0
Lawson Pascal Israel <isreallawson@outlook.com>
2024-01-08 00:06:15 +0000

    trying to fix output-code fail

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -5,12 +5,10 @@ def delete_at(my_list=[], idx=0):
     """
     bounds = len(my_list)
     if my_list:
-        if idx < 0:
+        if idx < 0 or my_list is None:
             return my_list
         elif idx >= bounds:
-            return my_list
-        elif my_list is None:
-            return my_list
+            return my_listt
         else:
             del my_list[idx]
         return my_list
