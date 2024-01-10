6be54eb312ff6998882277d24f14c8e352e26bea
Lawson Pascal Israel <isreallawson@outlook.com>
2024-01-10 23:39:58 +0000

    a function that returns a set of common elements in two sets

---
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def common_elements(set_1, set_2):
+    """
+    Returns a set of common elements within two sets.
+
+    set_1: The first set.
+    set_2: The second set.
+
+    Returns: Common elements.
+    """
+    return set_1.intersection(set_2)
