4f0fe25d862538ca0aa01bec243c03977ce42c06
Lawson Pascal Israel <isreallawson@outlook.com>
2024-01-11 00:36:48 +0000

    a function that list all values multiplied by a number without using loops

---
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def multiply_list_map(my_list=[], number=0):
+    """
+    Multiplies each element in a list by a specified number using the map
+    function.
+
+    my_list: The input list. Defaults to an empty list.
+    number: The multiplier. Defaults to 0.
+
+    Returns: A new list with elements multiplied by the specified number.
+    """
+    if my_list is None:
+        my_listt = []
+    return list(map(lambda value: value * number, my_list))
