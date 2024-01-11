a1b9d8b8d68cf21cc25cce54c64badd075852edf
Lawson Pascal Israel <isreallawson@outlook.com>
2024-01-11 00:39:15 +0000

    fixed code output-fail

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -11,4 +11,4 @@ def multiply_list_map(my_list=[], number=0):
     """
     if my_list is None:
         my_listt = []
-    return list(map(lambda value: value * number, my_list))
+    return list(map(lambda x: x * number, my_list))
