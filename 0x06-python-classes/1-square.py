5e6b88974220ce8b46772af503c93a3345626e96
Lawson Pascal Israel <isreallawson@outlook.com>
2024-01-23 18:40:57 +0000

    a class Square that defines a square by: (based on 0-square.py)

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+a class Square that defines a square by: (based on 0-square.py) with private
+instance atrribute 'size'
+"""
+
+
+class Square:
+    """"An empty class that defines a square"""
+    def __init__(self, size):
+        self.__size = size
+        pass
