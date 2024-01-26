bd9f4d07bb9852897d0771eed10446d8afdf700c
Lawson Pascal Israel <isreallawson@outlook.com>
2024-01-26 16:32:34 +0000

    added module description

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,7 +1,18 @@
 #!/usr/bin/python3
-"""A function that adds 2 integers"""
+"""
+This module features a function designed to calculate the sum of two numbers,
+primarily focusing on integers while gracefully accommodating float values. In
+cases where a float is provided as an argument, it undergoes conversion to an
+integer before the operation is executed. It's crucial to note that this
+conversion results in the loss of the decimal component, with only the integral
+part contributing to the final output.
+Beyond the specified argument types, the function diligently identifies and
+handles unsupported inputs, raising errors when encountered.
+"""
+
 
 def add_integer(a, b=98):
+
     """
     Adds two integers and returns the result (integer).
 
