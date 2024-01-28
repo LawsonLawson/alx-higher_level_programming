3841c1ea1028e22a28d1da7b953379be86de41ca
Lawson Pascal Israel <isreallawson@outlook.com>
2024-01-28 02:14:45 +0000

    a function that prints a text with 2 new lines after each of these characters: ., ? and :

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,24 @@
+"""
+    This module features a text_indentation function which checks if input is
+    valid and adds two new lines after any instances of `?`, `.` or `:`.
+    Prints the result without spaces at the beginning or end of sentences.
+"""
+
+
+def text_indentation(text):
+    """
+    Prints the text with two new lines after each '.', '?' or ':'.
+    When 'text is not a string, a TypeError is raised.
+
+    Args:
+    - text (str): The input string.
+    """
+    # Validate input
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    # Add two new lines after `?`, `.` or `:`
+    new_text = "".join([x if x not in "?.:" else x + "\n\n" for x in text])
+
+    # Remove spaces at the beginning or end of sentences and print the result
+    print("\n".join([line.strip() for line in new_text.split("\n")]), end="")
