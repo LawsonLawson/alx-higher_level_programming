365322402f7f55b1f30daefb4b0a13ec1b92c24f
Lawson Pascal Israel <isreallawson@outlook.com>
2024-01-31 11:44:55 +0000

    not sure what is causing the code output failure

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -3,32 +3,20 @@
 This module features a function designed to calculate the sum of two numbers,
 primarily focusing on integers while gracefully accommodating float values. In
 cases where a float is provided as an argument, it undergoes conversion to an
-integer before the operation is executed. It's crucial to note that this
-conversion results in the loss of the decimal component, with only the integral
-part contributing to the final output.
-Beyond the specified argument types, the function diligently identifies and
-handles unsupported inputs, raising errors when encountered.
+integer before the operation is executed.
 """
 
 
 def add_integer(a, b=98):
 
+    """Adds two integers and returns the result (integer).
+    Returns: int: The sum of the two numbers, casted to an integer.
     """
-    Adds two integers and returns the result (integer).
-
-    Args:
-    - a (int or float): The first integer.
-    - b (int or float): The second integer. Set to 98 by default.
-
-    Returns:
-    int: The sum of the two numbers, casted to an integer.
-    """
-    if not isinstance(a, int) and not isinstance(a, float):
+    if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
-    if not isinstance(b, int) and not isinstance(b, float):
+    elif not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
-    if isinstance(a, float):
-        a = int(a)
-    if isinstance(b, float):
-        b = int(b)
-    return (a + b)
+    a = int(a)
+    b = int(b)
+    return_value = a + b
+    return return_value
