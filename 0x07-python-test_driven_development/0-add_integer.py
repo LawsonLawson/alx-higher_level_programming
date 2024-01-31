687ea511621a98cbcc17ea79f5db0bba6835a9c4
Lawson Pascal Israel <isreallawson@outlook.com>
2024-01-31 11:22:17 +0000

    explicit checking args

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -23,11 +23,12 @@ def add_integer(a, b=98):
     Returns:
     int: The sum of the two numbers, casted to an integer.
     """
-    if not (isinstance(a, (int, float))):
-        raise TypeError('a must be an integer')
-    if not (isinstance(b, (int, float))):
-        raise TypeError('b must be an integer')
-    a = int(a)
-    b = int(b)
-
-    return a + b
+    if not isinstance(a, int) and not isinstance(a, float):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, int) and not isinstance(b, float):
+        raise TypeError("b must be an integer")
+    if isinstance(a, float):
+        a = int(a)
+    if isinstance(b, float):
+        b = int(b)
+    return (a + b)
