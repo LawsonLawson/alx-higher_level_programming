cf06522695e710bd431bcda58e1dcf2d7e227e28
Lawson Pascal Israel <isreallawson@outlook.com>
2024-02-05 19:55:18 +0000

    an empty class BaseGeometry

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Module: 5-base_geometry
+
+Description:
+This module defines the class BaseGeometry, which is initially an empty class.
+
+Class:
+BaseGeometry - An initially empty class.
+
+"""
+
+
+class BaseGeometry:
+    """
+    Class BaseGeometry: Initially an empty class.
+    """
+    pass
