cef2409c3ac1731913461c87cdd4e38355733c9e
Lawson Pascal Israel <isreallawson@outlook.com>
2024-02-06 20:28:19 +0000

    a class Student that defines a student by: (based on 9-student.py)

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+Module: 10-student.py
+
+Description: from 9-student.py module, instantiations were added in this module
+"""
+
+
+class Student:
+    """
+    Defines a student with first name, last name, and age.
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """
+        Initializes a Student instance with first name, last name, and age.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Retrieves a dictionary representation of a Student instance.
+        """
+        if attrs is None:
+            return self.__dict__
+        json_dict = {}
+        for attr in attrs:
+            if hasattr(self, attr):
+                json_dict[attr] = getattr(self, attr)
+        return json_dict
