0e6340297b2de18e22d102d9513076bea2af94bf
Lawson Pascal Israel <isreallawson@outlook.com>
2024-02-11 13:31:17 +0000

    added a cls method "save_to_file" that writes the JSON str rep

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -16,6 +16,8 @@ class Base:
     - __init__: Initializes the Base instance with an identifier.
     - to_json_string: Returns the JSON string representation of a list of
     dictionaries.
+    - save_to_file: Write the JSON string representation of list_objs to a
+    file.
     """
     __nb_objects = 0
 
@@ -47,3 +49,19 @@ class Base:
             return '[]'
         else:
             return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """
+        Write the JSON string representation of list_objs to a file.
+
+        Args:
+        - list_objs: List of objects inheriting from Base.
+        """
+        file = cls.__name__ + '.json'
+        with open(file, 'w') as opened_file:
+            if list_objs is None:
+                opened_file.write('[]')
+            else:
+                list_dicts = [obj.to_dictionary() for obj in list_objs]
+                opened_file.write(cls.to_json_string(list_dicts))
