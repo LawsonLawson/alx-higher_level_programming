26e2b65d037c898ccc1da4b3704980634858a9b1
Lawson Pascal Israel <isreallawson@outlook.com>
2024-02-11 12:05:20 +0000

    added a public method "to_dictionary" that returns a dict rep of the "Rectangle" class

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -93,6 +93,21 @@ class Rectangle(Base):
             for key, value in kwargs.items():
                 setattr(self, key, value)
 
+    def to_dictionary(self):
+        """
+        Returns the dictionary representation of the Rectangle.
+
+        Returns:
+        - Dictionary representation of the rectangle instance.
+        """
+        return {
+            'id': self.id,
+            'width': self.width,
+            'height': self.height,
+            'x': self.x,
+            'y': self.y
+        }
+
     @property
     def width(self):
         """Getter for width."""
