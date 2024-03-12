ab94937d5566ff66f5aa2cd5bbf40d15252e7e4e
Lawson Pascal Israel <isreallawson@outlook.com>
2024-03-12 22:33:29 +0000

    fixed syntax error

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,5 +1,5 @@
 -- This script lists the number of records with the same score in table `second_table`
-SELECT score,
-COUNT(*) AS number
+SELECT score, COUNT(*) AS number
 FROM second_table
-GROUP BY score DESC
+GROUP BY score
+GROUP BY score DESC;
