9508cffcbe0018da44089431a122bd49e4bc2b0b
Lawson Pascal Israel <isreallawson@outlook.com>
2024-03-14 00:14:23 +0000

    We count the number of shows using COUNT(sg.show_id) instead of COUNT(s.id)

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,6 +1,6 @@
 -- This is a script that lists all genres from the `hbtn_0d_tvshows` database and displays the number of shows linked to each.
-SELECT tv_genres.name AS genre, COUNT(tv_show_genres.genre_id) AS number_shows
-FROM tv_show_genres
-INNER JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
-GROUP BY tv_genres.name
-ORDER BY 2 DESC;
+SELECT g.name AS genre, COUNT(sg.show_id) AS number_of_shows
+FROM genres g
+JOIN shows_genres sg ON g.id = sg.genre_id
+GROUP BY g.name
+ORDER BY number_of_shows DESC;
