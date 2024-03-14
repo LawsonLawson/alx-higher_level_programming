9c546aebd292b971c9f904c3aad76a3f0a7ef00b
Lawson Pascal Israel <isreallawson@outlook.com>
2024-03-14 00:07:45 +0000

    the LEFT JOIN ensures that all rows from the tv_genres table are included in the result set, even if there are no matches in the tv_show_genres table

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,6 +1,6 @@
 -- This is a script that lists all genres from the `hbtn_0d_tvshows` database and displays the number of shows linked to each.
 SELECT tv_genres.name AS genre, COUNT(tv_show_genres.genre_id) AS number_shows
-FROM tv_genres
-LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+FROM tv_show_genres
+INNER JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
 GROUP BY tv_genres.name
-ORDER BY number_shows DESC;
+ORDER BY 2 DESC;
