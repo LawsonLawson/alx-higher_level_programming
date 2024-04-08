6c9a809efee7ceb599929b85f0da24cab62d1dca
Lawson Pascal Israel <isreallawson@outlook.com>
2024-04-08 19:33:29 +0000

    Write a script that prints 3 lines

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+/**
+ * A script that prints 3 lines to the console:
+ *  “C is fun”
+ *  “Python is cool”
+ *  “JavaScript is amazing”
+ */
+const myVar1 = 'C is fun';
+const myVar2 = 'Python is cool';
+const myVar3 = 'JavaScript is amazing';
+
+console.log(myVar1, '\n' + myVar2, '\n' + myVar3);
