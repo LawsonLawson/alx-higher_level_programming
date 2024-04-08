0a3d4680241d2dbea4e79c1f7418f766d1288228
Lawson Pascal Israel <isreallawson@outlook.com>
2024-04-08 23:30:51 +0000

    a script that prints two arguments passed to it

---
+++ 0x12-javascript-warm_up/4-concat.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+/**
+ * A script that prints two arguments passed to it, in the following format:
+ * “ is ”
+ * Example - "arg_2" "is" "arg_3"
+ */
+console.log(process.argv[2] + ' is ' + process.argv[3]);
