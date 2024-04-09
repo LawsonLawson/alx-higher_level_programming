4b0724988d9f11dfd8defff42a2b85b4fa3e6cbe
Lawson Pascal Israel <isreallawson@outlook.com>
2024-04-09 22:59:47 +0000

    a function that returns the number of occurrences in a list

---
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+/**
+ * A function that returns the number of occurrences in a list.
+ * Prototype:
+ * `exports.nbOccurences = function (list, searchElement)`
+ */
+
+exports.nbOccurences = function (list, searchElement) {
+  let occurrences = 0;
+  for (let i = 0; i < list.length; i++) {
+    if (searchElement === list[i]) {
+      occurrences++;
+    }
+  }
+  return occurrences;
+};
