0c881e4a6d0e4517d45b1c02ee8f120e304903dc
Lawson Pascal Israel <isreallawson@outlook.com>
2024-04-11 18:46:17 +0000

    a script that lists all states from the database hbtn_0e_0_usa

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+'''
+This script retrieves and displays all states from the hbtn_0e_0_usa database.
+'''
+
+import sys
+import MySQLdb
+
+if __name__ = '__main__':
+
+    # Connect to the MySQL database
+    db_connection = MySQL.connect(user=sys.argv[1], passwd=sys.argv[2],
+                                  db=sys.argv[3], port=3306)
+
+    # Create a cursor object to interact with the database
+    db_cursor = db_connection.cursor()
+
+    # Execute the SQL query to retrieve all states
+    db_cursor.execute("SELECT * FROM states;")
+
+    # Fetch all the rows returned by the query
+    states_data = db_cursor.fetchall()
+
+    # Display each state
+    for state_record in states_data:
+        print(state_record)
