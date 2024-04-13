7b55b71e17732b878ceb98a7a79c7ef3184d05ad
Lawson Pascal Israel <isreallawson@outlook.com>
2024-04-13 00:20:43 +0000

    a script that lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+
+'''
+Script to list all states from the database 'hbtn_0e_0_usa'
+'''
+
+if __name__ == "__main__":
+    import sys
+    import MySQLdb
+
+    # Retrieve MySQL connection details from command line arguments
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
+
+    # Connect to the MySQL database
+    connection = MySQLdb.connect(
+        host="localhost",
+        user=mysql_username,
+        password=mysql_password,
+        database=database_name,
+        port=3306,
+    )
+
+    # Create a cursor object to execute queries
+    cursor = connection.cursor()
+
+    # SQL query to select all states
+    sql_query = """
+                SELECT * FROM states ORDER BY id;
+                """
+
+    # Execute the SQL query
+    cursor.execute(sql_query)
+
+    # Fetch all rows from the result set
+    result_set = cursor.fetchall()
+
+    # Print the results
+    for state_record in result_set:
+        print(state_record)
+
+    # Close cursor and database connection
+    cursor.close()
+    connection.close()
