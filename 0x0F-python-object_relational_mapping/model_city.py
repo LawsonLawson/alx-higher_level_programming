a8c4b1f02d662efb8c740163f38063b44444b4ff
Lawson Pascal Israel <isreallawson@outlook.com>
2024-04-13 03:01:06 +0000

    a Python file similar to model_state.py named model_city.py that contains the class definition of a City

---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+'''
+A module that contains the class definition of a City
+'''
+
+
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+
+
+class City(Base):
+    """
+    A new SQL table representing cities
+    """
+    __tablename__ = "cities"
+    id = Column("id", Integer(), autoincrement=True,
+                nullable=False, primary_key=True)
+    name = Column("name", String(128), nullable=False)
+    state_id = Column(Integer(), ForeignKey("states.id"), nullable=False)
