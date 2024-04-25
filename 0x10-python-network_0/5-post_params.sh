315d968b83c6947560d75ff17e94a5fff4e20a13
Lawson Pascal Israel <isreallawson@outlook.com>
2024-04-25 22:23:40 +0000

    not sure what I am missing

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script sends a POST request to a specified URL with custom headers
-curl -sX POST -d "emai=test@gmail.com&subject=I will always be here for PLD" "$1"
+curl -s -X POST -d "emai=test@gmail.com&subject=I will always be here for PLD" "$1"
