6a30359abc923b10c2ba81884ed89ac69d91802c
Lawson Pascal Israel <isreallawson@outlook.com>
2024-04-26 00:54:46 +0000

    fixed typo

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script sends a POST request to a specified URL with custom headers
-curl -s -X POST -d "emai=test@gmail.com&subject=I will always be here for PLD" "$1"
+curl -s -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
