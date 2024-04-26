7f3f571a7ad310b937e3e71144f609971c74d9e7
Lawson Pascal Israel <isreallawson@outlook.com>
2024-04-26 12:30:08 +0000

    a Python script that takes in a URL, sends a request to the URL and displays the value of the variable X-Request-Id in the response header

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+This script sends a GET request to a specified URL and prints the value of the
+'X-Request-Id' header from the response.
+"""
+
+# Importing the 'sys' module for handling command-line arguments
+import sys
+
+# Importing the 'requests' module for handling HTTP requests
+import requests
+
+if __name__ == "__main__":
+    # Send a GET request to the URL specified in the command-line arguments
+    r = requests.get(sys.argv[1])
+
+    # Print the value of the 'X-Request-Id' header from the response
+    print(r.headers.get('X-Request-Id'))
